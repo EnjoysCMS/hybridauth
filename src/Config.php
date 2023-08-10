26ada17c5e544e7c2d34851b48148138a4cf2000
enjoys <deadly.info@gmail.com>
2023-08-10 12:18:55 +0300

    fix config

--- src/Config.php
+++ src/Config.php
@@ -9,43 +9,21 @@ final class Config
 
     private const MODULE_NAME = 'enjoyscms/hybridauth';
 
-
-    public function __construct(
-        private readonly \Enjoys\Config\Config $config,
-//        private Container $container,
-//        private Session $session,
-//        private EntityManager $em,
-//        private LoggerInterface $logger,
-//        ModuleCollection $moduleCollection
-    )
+    public function __construct(private readonly \Enjoys\Config\Config $config)
     {
-//
-
-
-//        if (file_exists($module->path . '/config.yml')) {
-//            $config->addConfig(
-//                [
-//                    self::MODULE_NAME => file_get_contents($module->path . '/config.yml')
-//                ],
-//                ['flags' => Yaml::PARSE_CONSTANT],
-//                \Enjoys\Config\Config::YAML,
-//                false
-//            );
-//        }
     }
 
     public function get(string $key = null, mixed $default = null): mixed
     {
         if ($key === null) {
-            return $this->config->get(self::MODULE_NAME);
+            return $this->all();
         }
         return $this->config->get(sprintf('%s->%s', self::MODULE_NAME, $key), $default);
     }
 
-
     public function all(): array
     {
-        return $this->config->get();
+        return $this->config->get(self::MODULE_NAME);
     }
 
 }
